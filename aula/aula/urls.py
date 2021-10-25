4a8696f6574e41309ce027ab4e6560809f09b0e7
leticia-brand <leticia.brandao.gs@gmail.com>
2021-10-25 09:45:01 -0300

    updating urls

--- aula/aula/urls.py
+++ aula/aula/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', fgv_views.fgv, name = "fgv"),
+    path(r"fgv/", include("fgv.urls"))
 ]
