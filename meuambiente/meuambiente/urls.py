8ca6061f7b2dfc9a0603e03141b34e7cf4e22468
marcelimelchiors <marcelimelchiors12@gmail.com>
2021-10-27 08:28:00 -0300

    alterações no requirements

--- meuambiente/meuambiente/urls.py
+++ meuambiente/meuambiente/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", myfgv_views.myfgv,name="myfgv"),
     path(r"myfgv/",include("myfgv.urls"))
 ]
