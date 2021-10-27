c75841b3c29aa16b5c5ea357aade4580fb80f017
leticia-brand <leticia.brandao.gs@gmail.com>
2021-10-27 08:53:33 -0300

    adding urls

--- aula/aula/urls.py
+++ aula/aula/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import aulas.views 
+from django.urls import path, include
+import polls.views as p_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', aula.views.index),
+    path('',pviews.index)
     
 ]
