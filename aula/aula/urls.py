d1c80b94d3704d3a51d91c8223c7e20c9529e8a7
leticia-brand <leticia.brandao.gs@gmail.com>
2021-10-26 21:40:24 -0300

    consertando erros

--- aula/aula/urls.py
+++ aula/aula/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', fgv_views.fgv, name = "fgv"),
-    path(r"fgv/", include("fgv.urls"))
+    path('admin/', admin.site.urls),
 ]
